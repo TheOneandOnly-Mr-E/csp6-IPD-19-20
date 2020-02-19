998d104de8f1fe7f740311fb0352cb1c38f1ec35
TheOneandOnly-Mr-E <aaronjsmith2021@gmail.com>
2020-02-19 22:45:58 +0000

    Added final strategy to main branch

--- team8.py
+++ team8.py
@@ -5,43 +5,18 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-import random
-import prisoners_dilemma.py
 
-team_name = 'Rush Hour' # Only 10 chars displayed.
-strategy_name = 'Betray most of the time and collude randomly'
-strategy_description = 'A random generator from '
+team_name = 'Rush Hour'
+strategy_name = 'History Manipulation'
+strategy_description = 'Convert all of their bs to cs'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
+  'changes their history to all collude while ours is all betray with one collude at the start'
+  if len(my_history) == 0:
+    return 'c'
+  else:
+    return 'b'
 
-    random_value = random.randint(1,number_of_rounds)
-    halfway_value = number_of_rounds/2
-    if random_value < halfway_value:
-      for i in my_history:
-        if i % random_value == 0:
-          return 'c'
-        else:
-          return 'b'
-
-  
-
-
-
-
-
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-
+  for i in their_history:
+    if i == 'b':
+      i = 'c'
