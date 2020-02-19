a2ba3c5b1c07096fb1375b29fa1d890b924f37b3
kaishuf <kaishuf3@gmail.com>
2020-02-19 21:37:15 +0000

    example #0 example #1

--- example1.py
+++ example1.py
@@ -11,15 +11,11 @@ strategy_name = 'Betray'
 strategy_description = 'Always betray.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    #This example player always betrays.      
-    return 'b'
+  random_value = random.randint(1,number_of_rounds)
+  halfway_value = number_of_rounds/2
+  if random_value < halfway_value:
+    for i in my_history:
+      if i % random_value == 0:
+        return 'c'
+      else:
+        return 'b'
